fe9fc002aacbdbdc43a410adca9ef561f9a3fdd4
Devweb139 <goualnabil691@gmail.com>
2021-03-04 11:14:26 +0100

    update pageObject.js

--- cypress/integration/partie4/pageObject.js
+++ cypress/integration/partie4/pageObject.js
@@ -6,91 +6,62 @@ import homePage from "../../pageObject/sauceDemo/homePage";
 const invent = new inventoryPage
 const homeP = new homePage
 
-describe("Test sur les pages object", () => {
+context("Test sur les pages object", () => {
 
-    beforeEach(function() {
+    beforeEach(function () {
         cy.visit('https://www.saucedemo.com/')
 
         cy.fixture('login').as("login")
-    }) 
-
-
-    it.skip('test de connexion - OK', () => {
-
-        homeP.usernameInput().type('standard_user')
-        homeP.passwordInput().type('secret_sauce')
-        homeP.loginButton().click()
-        // assertions
-        cy.url().should('contain','https://www.saucedemo.com/inventory.html')
-        
     })
 
-    it.skip('test de connexion - locked out', () => {
+    describe("Tests without custom command", () => {
 
-        homeP.usernameInput().type('locked_out_user')
-        homeP.passwordInput().type('secret_sauce')
-        homeP.loginButton().click()
-        // assertions
-        homeP.errorMessageForm().should('be.visible')
-        cy.url().should('contain','https://www.saucedemo.com/')
-        
-    })
+        it('test de connexion - OK', () => {
 
-    it('test de connexion - OK - fixtures', () => {
+            homeP.usernameInput().type('standard_user')
+            homeP.passwordInput().type('secret_sauce')
+            homeP.loginButton().click()
+            // assertions
+            cy.url().should('contain', 'https://www.saucedemo.com/inventory.html')
 
-        cy.get('@login').then((loginData) => {
-            cy.loginForm(loginData.standard_user)
         })
-        
-        // assertions
-        cy.url().should('contain','https://www.saucedemo.com/inventory.html')
-        
-    })
 
-    it('test de connexion - problem_user - fixtures', () => {
+        it('test de connexion - locked out', () => {
 
-        cy.get('@login').then((loginData) => {
-            cy.loginForm(loginData.problem_user)
+            homeP.usernameInput().type('locked_out_user')
+            homeP.passwordInput().type('secret_sauce')
+            homeP.loginButton().click()
+            // assertions
+            homeP.errorMessageForm().should('be.visible')
+            cy.url().should('contain', 'https://www.saucedemo.com/')
+
         })
-        
-        // assertions
-        cy.url().should('not.contain','https://www.saucedemo.com/inventory.html/inventory.html')
-        
+
     })
 
+    describe("Tests with custom commands", () => {
 
-    it.skip('test', () => {
+        it('test de connexion - OK standart_user - fixtures', () => {
 
-        cy.visit('/inventory.html')
+            cy.get('@login').then((loginData) => {
+                cy.loginForm(loginData.standard_user)
+            })
 
-        cy.ajouterTousLesProduits()
-        let nbrTotal = 6
+            // assertions
+            cy.url().should('contain', 'https://www.saucedemo.com/inventory.html')
 
-        // Voir le panier
-        // cy.get("#shopping_cart_container").click()
-        invent.buttonPanier().click()
-        cy.url().should('contain', '/cart.html')
+        })
 
-        // Calculer le nombre d'articles dans le panier
-        cy.calcNbrArticlePanier(nbrTotal)
+        it('test de connexion - problem_user - fixtures', () => {
 
-        // Valider le panier
-        cy.get(".btn_action.checkout_button").click()
-        cy.url().should('contain', '/checkout-step-one.html')
+            cy.get('@login').then((loginData) => {
+                cy.loginForm(loginData.problem_user)
+            })
 
-        // Remplir le formulaire 
-        cy.formulaireCheckout("Gowall", "Nabil", "75111")
+            // assertions
+            cy.url().should('not.contain', 'https://www.saucedemo.com/inventory.html/inventory.html')
 
-        // ceci est utile pour :
-        // Rendre les test plus lisibles
-        // réutiliser cette commande ailleurs
-        // tester différend jeux de données
-
-        // Finaliser l'achat
-        cy.get(".btn_action").contains("FINISH").click()
-        cy.url().should('contain', '/checkout-complete.html')
-        cy.get(".checkout_complete_container").should('contain', 'THANK YOU FOR YOUR ORDER')
-        
+        })
 
     })
 
